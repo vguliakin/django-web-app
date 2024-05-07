fe33c449179d9dcfdeab1deb6f3c08987165da95
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-07 13:31:31 -0400

    Change the home page view

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,9 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows),
+                            "New to-do item did not appear in table",
+                        )
 
         # There is still a text box inviting user to add another item.
         # User enters "Use peacock feathers to make a fly"
