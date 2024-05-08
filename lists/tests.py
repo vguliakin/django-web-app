c7b983f54a23055f4356c23bfd1f221a8bb9c3da
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-07 22:44:41 -0400

    Redirect after method post

--- lists/tests.py
+++ lists/tests.py
@@ -11,8 +11,16 @@ class HomePageTest(TestCase):
     
     def test_cam_save_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, "home.html")
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
+
+    if request.method == "POST":
+        item = Item()
+        item.text = request.POST["item_text"]
+        item.save()
+        return redirect("/")
+
     return render(
         request,
         "home.html",
