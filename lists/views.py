afc274401316f570960b0bba23eac6dac67fc37c
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-10 11:29:33 -0400

    Change action path on form

--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,6 @@ from lists.models import Item
 
 
 def home_page(request):
-
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/first-list/")
-
     return render(request, "home.html")
 
 def view_list(request):
