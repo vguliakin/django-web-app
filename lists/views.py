9b792960201c13239662a2a63a78de74df932af9
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-10 10:35:58 -0400

    Change home page view

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,7 @@ def home_page(request):
         Item.objects.create(text=request.POST["item_text"])
         return redirect("/lists/first-list/")
 
-    items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+    return render(request, "home.html")
 
 def view_list(request):
     items = Item.objects.all()
