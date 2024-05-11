3e520a93b473b158226621ed6b549851cd0b53c5
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-11 11:50:45 -0400

    new URL, view for adding to existing lists

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, "home.html")
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request, list_id):
+    list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": list})
 
 def new_list(request):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/first-list/")
+    return redirect(f"/lists/{nulist.id}/")
+
+def add_item(request, list_id):
+    list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list)
+    return redirect(f"/lists/{list.id}/")
--- superlists/urls.py
+++ superlists/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
     path('lists/new', views.new_list, name='new_list'),
-    path('lists/first-list/', views.view_list, name="view_list"),
+    path('lists/<int:list_id>/', views.view_list, name="view_list"),
+    path('lists/<int:list_id>/add_item', views.add_item, name="add_item"),
 ]
